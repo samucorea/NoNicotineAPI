25853e66741a55a57541ad6567ba1cc2da9cb5a0
Emmanuel Cuevas <emmanuelcuevasm1@gmail.com>
2022-09-12 00:20:28 -0400

    feat: implement CQRS & MediatR design patterns

---
+++ NoNicotineAPI/Controllers/PatternsExample.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NoNicotin_Business.Queries;
+
+namespace NoNicotineAPI.Controllers
+{
+    public class PatternsExample : Controller
+    {
+        public PatternsExample(IMediator mediator) => _mediator = mediator;
+        private readonly IMediator _mediator;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetProducts()
+        {
+            var entities = await _mediator.Send(new GetExampleEntitiesQuery());
+
+            return Ok(entities);
+        }
+
+    }
+}
---
+++ NoNicotine_Data/Entities/ExampleEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoNicotine_Data.Entities
+{
+    public class ExampleEntity
+    {
+        public string ID { get; set; }
+    }
+}
