542c6dedce59ddba994c4faf4946922196170231
Emmanuel Cuevas <emmanuelcuevasm1@gmail.com>
2022-09-12 00:42:16 -0400

    feat: implement DI for Designs patterns & EF

--- NoNicotineAPI/Program.cs
+++ NoNicotineAPI/Program.cs
@@ -1,7 +1,12 @@
+using MediatR;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using NoNicotine_Data.Context;
 using NoNicotineAPI;
 using Serilog;
 using Serilog.Sinks.MSSqlServer;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +16,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//MediatR
+builder.Services.AddMediatR(typeof(Program));
 
 string sqlServerConnectionString = builder.Configuration.GetConnectionString("local");
-
-builder.Services.AddDbContext<NoNicotineContext>(opt => opt.UseSqlServer(sqlServerConnectionString));
+//builder.Services.AddDbContext<NoNicotineContext>(opt => opt.UseSqlServer(sqlServerConnectionString));
+builder.Services.AddDbContext<AppDbContext>(opts =>
+    opts.UseSqlServer("Data Source=localhost;Initial Catalog=test1;Integrated Security=True"));
 
 
 var logger = new LoggerConfiguration()
