614e5ba3bdc955bf218bd06003c553650c3b226b
Simon Garcia <gasimon12@gmail.com>
2022-09-20 20:17:05 -0400

    feat: add create therapist endpoint
    Add create therapist endpoint to API Project to be able to add a new
    therapist to the database. This implies the creation of a CreateTherapistCommand
    and it's corresponding handler. Also, minor modifications to the Therapist class.

---
+++ NoNicotin_Business/Commands/CreateTherapistCommand.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using NoNicotin_Business.Value_Objects;
+using NoNicotine_Data.Entities;
+using NoNicotineAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoNicotin_Business.Commands
+{
+    public class CreateTherapistCommand:IRequest<Response<Therapist>>
+    {
+        public string? Name { get; set; }
+        public char Sex { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+        public string? Identification { get; set; }
+
+        public string IdentificationTherapistType { get; set; } = IdentificationType.IDENTIFICATION_CARD;
+    }
+}
---
+++ NoNicotineAPI/Controllers/TherapistsController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NoNicotin_Business.Commands;
+
+namespace NoNicotineAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TherapistsController : Controller
+    {
+        public TherapistsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        private readonly IMediator _mediator;
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTherapist(CreateTherapistCommand request)
+        {
+            var result = await _mediator.Send(request);
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
