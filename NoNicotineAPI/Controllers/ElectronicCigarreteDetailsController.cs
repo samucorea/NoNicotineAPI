4e7dc2605e9af48e3dd7c2f7108081a701cedc6c
Emmanuel Cuevas <emmanuelcuevasm1@gmail.com>
2022-10-15 20:51:03 -0400

    feat: create controller for electonic cigarrete
    create controller for electonic cigarrete details
    
    AB#123

---
+++ NoNicotineAPI/Controllers/ElectronicCigarreteDetailsController.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NoNicotine_Business.Commands;
+using NoNicotine_Business.Queries;
+using System.Data;
+
+namespace NoNicotineAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "patient")]
+    public class ElectronicElectronicCigarreteDetailsController : Controller
+    {
+
+        public ElectronicElectronicCigarreteDetailsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        private readonly IMediator _mediator;
+
+        [HttpPost]
+        public async Task<IActionResult> CreateElectronicCigarreteDetail(CreateElectronicCigarreteDetailsCommand request)
+        {
+
+            var result = await _mediator.Send(request);
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetElectronicCigarreteDetail(GetElectronicCigarreteDetailQuery request)
+        {
+
+            var result = await _mediator.Send(request);
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateElectronicCigarreteDetail(UpdateElectronicCigarreteDetailsCommand request)
+        {
+            var result = await _mediator.Send(request);
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
