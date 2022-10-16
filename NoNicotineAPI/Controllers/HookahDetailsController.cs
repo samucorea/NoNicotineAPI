16070b9fe11b1dca40b5f6c1cb0766509e11e4f1
Emmanuel Cuevas <emmanuelcuevasm1@gmail.com>
2022-10-15 20:53:22 -0400

    feat: create controller for hookah details
    AB#123

---
+++ NoNicotineAPI/Controllers/HookahDetailsController.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NoNicotine_Business.Commands;
+using NoNicotine_Business.Queries;
+using System.Data;
+
+namespace NoNicotineAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "patient")]
+    public class HookahDetailsController : Controller
+    {
+        public HookahDetailsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        private readonly IMediator _mediator;
+
+        [HttpPost]
+        public async Task<IActionResult> CreateHookahDetail(CreateHookaDetailsCommand request)
+        {
+
+            var result = await _mediator.Send(request);
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetHookahDetail(GetHookahDetailsQuery request)
+        {
+
+            var result = await _mediator.Send(request);
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateHookahDetail(UpdateHookaDetailsCommand request)
+        {
+            var result = await _mediator.Send(request);
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
