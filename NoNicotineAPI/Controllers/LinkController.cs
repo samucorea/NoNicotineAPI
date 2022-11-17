c202a6114a28a143de1b5bb1c94e5108ebd42409
Emmanuel Cuevas <emmanuelcuevasm1@gmail.com>
2022-11-16 21:36:32 -0400

    feat: add link controller
    AB#123

---
+++ NoNicotineAPI/Controllers/LinkController.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NoNicotine_Business.Commands;
+using System.Data;
+
+namespace NoNicotineAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "patient")]
+    public class LinkController : Controller
+    {
+        public LinkController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        private readonly IMediator _mediator;
+
+        [HttpPut]
+        public async Task<IActionResult> AcceptDenyLinkRequest(UpdateAcceptDenyLinkrequestCommand request)
+        {
+
+            var result = await _mediator.Send(request);
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UnrelatePatientLink(UpdateUnrelatePatientTherapistCommand request)
+        {
+
+            var result = await _mediator.Send(request);
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
