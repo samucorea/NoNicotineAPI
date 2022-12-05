3e8c267c0543a7fa77105c68eee44f71ff7752ae
samucorea <samuel.alejandro100@hotmail.com>
2022-12-05 14:49:12 -0400

    fix: convert string to list in entries

--- NoNicotine_Business/Repositories/EntryRepository.cs
+++ NoNicotine_Business/Repositories/EntryRepository.cs
@@ -10,65 +10,71 @@ using System.Threading.Tasks;
 
 namespace NoNicotine_Business.Repositories
 {
-    public class EntryRepository : IEntryRepository
+  public class EntryRepository : IEntryRepository
+  {
+    private readonly AppDbContext _context;
+    public EntryRepository(AppDbContext context)
     {
-        private readonly AppDbContext _context;
-        public EntryRepository(AppDbContext context)
-        {
-            _context = context;
-        }
-
-        public async Task<bool> CreateEntryAsync(Entry entry, CancellationToken cancellationToken)
-        { 
-
-            await _context.Entry.AddAsync(entry, cancellationToken);
-
-            return await _context.SaveChangesAsync(cancellationToken) == 1;
-        }
-
-        public async Task<Entry?> GetPatientEntryByIdAsync(string patiendId, string entryId, CancellationToken cancellationToken)
-        {
-            var entry = await _context.Entry.Where(entry => entry.ID == entryId && entry.PatientId == patiendId).FirstOrDefaultAsync(cancellationToken);
-            if (entry == null)
-            {
-                return null;
-            }
-
-            return entry;
-        }
-        public async Task<List<Entry>> GetPatientEntriesAsync(string patiendId, CancellationToken cancellationToken)
-        {
-            var entries = await _context.Entry.Where(entry => entry.PatientId == patiendId).ToListAsync(cancellationToken);
-
-            return entries;
-        }
-        public async Task<List<SharedEntry>?> GetPatientSharedEntriesAsync(string patiendId, CancellationToken cancellationToken)
-        {
-            var entries = await _context.Entry.Where(entry => entry.PatientId == patiendId && entry.TherapistAllowed).ToListAsync(cancellationToken);
-            
-            if (entries.Count < 1)
-            {
-                return null;
-            }
-
-            List<SharedEntry> sharedEntries = new List<SharedEntry>();
-
-            foreach (var entry in entries)
-            {
-                SharedEntry sharedEntry = new()
-                {
-                    Message = entry.Message,
-                    TherapistAllowed = entry.TherapistAllowed,
-                    PatientId = entry.PatientId,
-                    Symptoms = entry.Symptoms,
-                    Feelings = entry.Feelings,
-                    ID = entry.ID,
-                    CreatedAt = entry.CreatedAt,
-                };
-                sharedEntries.Add(sharedEntry);
-            }
-
-            return sharedEntries;
-        }
+      _context = context;
     }
+
+    public async Task<bool> CreateEntryAsync(Entry entry, CancellationToken cancellationToken)
+    {
+
+      await _context.Entry.AddAsync(entry, cancellationToken);
+
+      return await _context.SaveChangesAsync(cancellationToken) == 1;
+    }
+
+    public async Task<Entry?> GetPatientEntryByIdAsync(string patiendId, string entryId, CancellationToken cancellationToken)
+    {
+      var entry = await _context.Entry.Where(entry => entry.ID == entryId && entry.PatientId == patiendId).FirstOrDefaultAsync(cancellationToken);
+      if (entry == null)
+      {
+        return null;
+      }
+
+      return entry;
+    }
+    public async Task<List<Entry>> GetPatientEntriesAsync(string patiendId, CancellationToken cancellationToken)
+    {
+      var entries = await _context.Entry.Where(entry => entry.PatientId == patiendId).ToListAsync(cancellationToken);
+
+      return entries;
+    }
+    public async Task<List<SharedEntry>?> GetPatientSharedEntriesAsync(string patiendId, CancellationToken cancellationToken)
+    {
+      var entries = await _context.Entry.Where(entry => entry.PatientId == patiendId && entry.TherapistAllowed).ToListAsync(cancellationToken);
+
+      if (entries.Count < 1)
+      {
+        return null;
+      }
+
+      List<SharedEntry> sharedEntries = new List<SharedEntry>();
+
+      foreach (var entry in entries)
+      {
+        SharedEntry sharedEntry = new()
+        {
+          Message = entry.Message,
+          TherapistAllowed = entry.TherapistAllowed,
+          PatientId = entry.PatientId,
+          Symptoms = MapStringToList(entry.Symptoms),
+          Feelings = MapStringToList(entry.Feelings),
+          ID = entry.ID,
+          CreatedAt = entry.CreatedAt,
+        };
+        sharedEntries.Add(sharedEntry);
+      }
+
+      return sharedEntries;
+    }
+    private List<string> MapStringToList(string list)
+    {
+      return list.Split(',').ToList();
+    }
+  }
+
+
 }
